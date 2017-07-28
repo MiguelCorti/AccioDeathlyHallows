fcfba46a4684b73f0c88fb0b7a14787c2c373a9e
Miguel Corti <miguelszcorti@gmail.com>
2017-07-27 22:15:48 -0300

    Added one obstacle, fixed background and a score bar

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour {
 
 	public static GameControl instance;
+	public GameObject scoreBar; 
 	public bool gameOver = false;
 	public float scrollSpeed = -1.5f;
+
+	private Animator scoreAnim;
 
 	// Use this for initialization
 	void Awake () 
@@ -17,6 +21,8 @@ public class GameControl : MonoBehaviour {
 		} else if (instance != this) {
 			Destroy (gameObject);
 		}
+
+		scoreAnim = scoreBar.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -26,6 +32,16 @@ public class GameControl : MonoBehaviour {
 		{
 			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
 		}
+	}
+
+	public void PlayerScored()
+	{
+		if (gameOver) 
+		{
+			return;
+		}
+
+		scoreAnim.SetTrigger ("Scores");
 	}
 
 	public void PlayerDied()
